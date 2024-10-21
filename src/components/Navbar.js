4d769dd798fbf0433d7d95f62ef5c59db400ab74
unsoul7 <disketteit@gmail.com>
2024-10-22 05:07:25 +0530

    still need work in responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import cart from '../assets/cart.png'
 import '../styles/nav.css'
 import { Link } from "react-router-dom";
 import logo from "../assets/logo-me.svg"
+import menu from '../assets/menu.png'
+import close from '../assets/close.png'
 
 export const Navbar = () => {
+    const [Nav, setNav] = useState('hidden')
+
+    const openNav = () => {
+        if (Nav == '') {
+            setNav('hidden')
+        }else{
+            setNav('')
+        }
+    }
+
     return (
         <nav>
             <img id="logo" src={logo} alt="" />
-            <div>
+            <img className="menu-btn" onClick={() => {openNav()}} src={menu} alt="" />
+            <div className={Nav}>
                 <Link className="nav-btn" to='#'>HOME</Link>
                 <Link className="nav-btn" to='#'>SHOP</Link>
                 <Link className="nav-btn" to='#'>ABOUT</Link>
